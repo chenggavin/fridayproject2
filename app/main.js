7b1fcc97745eee4495d1e94fe1fc9b98153739b5
Gavin Cheng <gavincheng01@gmail.com>
2017-10-13 16:24:56 -0400

    finsihed MVP

--- app/main.js
+++ app/main.js
@@ -54,8 +54,6 @@ var sortinghat = new Vue({
 
 		  ],
 
-
-
 	},
 
 
@@ -65,11 +63,24 @@ var sortinghat = new Vue({
 		showstuff: function() {
 			this.currenthouse.showing = true;
 			console.log('showstuff is hit');
-		}
+		},
+
+	    loadContacts: function() {
+
+	     	this.currenthouse = localStorage.getItem('currenthouse');
+	     
+
+	    },
+
+		saveContacts: function() {
+
+		    localStorage.setItem('currenthouse', JSON.stringify(this.currenthouse));
 
 	
 
-},
+		},
+	},
+
 
 	beforeMount: function() {
 
@@ -85,3 +96,143 @@ var sortinghat = new Vue({
 
 
 });
+
+var houseswitch = new Vue ({
+
+
+	
+
+
+
+	data: {
+
+		Questions: [
+
+		    { 
+		      text: 'What is your favorite color?',
+		      answers: [
+		        {
+		          text: 'Scarlet',
+		          house: 'Gryffindor'
+		        },
+		        {
+		          text: 'Yellow',
+		          house: 'Hufflepuff'
+		        },
+		        {
+		          text: 'Blue',
+		          house: 'Ravenclaw'
+		        },
+		        {
+		          text: 'Green',
+		          house: 'Slytherin'
+		        }
+		      ]
+		    },
+		    { 
+		      text: 'What is your favorite animal?',
+		      answers: [
+		        {
+		          text: 'Lion',
+		          house: 'Gryffindor'
+		        },
+		        {
+		          text: 'Badger',
+		          house: 'Hufflepuff'
+		        },
+		        {
+		          text: 'Eagle',
+		          house: 'Ravenclaw'
+		        },
+		        {
+		          text: 'Snake',
+		          house: 'Slytherin'
+		        }
+		      ]
+		    },
+		    { 
+		      text: 'What kind of person do you try to be?',
+		      answers: [
+		        {
+		          text: 'Brave',
+		          house: 'Gryffindor'
+		        },
+		        {
+		          text: 'Loyal',
+		          house: 'Hufflepuff'
+		        },
+		        {
+		          text: 'Witty',
+		          house: 'Ravenclaw'
+		        },
+		        {
+		          text: 'Cunning',
+		          house: 'Slytherin'
+		        }
+		      ]
+		    },
+		    { 
+		      text: 'Who is your favorite witch or wizard?',
+		      answers: [
+		        {
+		          text: 'Albus Dumbledore',
+		          house: 'Gryffindor'
+		        },
+		        {
+		          text: 'Newt Scamander',
+		          house: 'Hufflepuff'
+		        },
+		        {
+		          text: 'Garrick Ollivander',
+		          house: 'Ravenclaw'
+		        },
+		        {
+		          text: 'Tom Riddle',
+		          house: 'Slytherin'
+		        }
+		      ]
+		    },
+		    { 
+		      text: 'What is your favorite Berty Bott\'s flavor?',
+		      answers: [
+		        {
+		          text: 'Grape',
+		          house: 'Gryffindor'
+		        },
+		        {
+		          text: 'Horseradish',
+		          house: 'Hufflepuff'
+		        },
+		        {
+		          text: 'Roast Beef',
+		          house: 'Ravenclaw'
+		        },
+		        {
+		          text: 'Spinach',
+		          house: 'Slytherin'
+		        }
+		      ]
+		    }
+
+		]
+	}
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
